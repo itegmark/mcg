4ef46a206cf008babdbf42936c783666765eb79a
ilya on cachyos <conversioncrossing@gmail.com>
2024-08-14 18:59:48 +0300

    deploying to production

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SERVER_IP="147.45.163.100"
+REPO_URL="https://github.com/itegmark/mcg.git"
+BRANCH="main"
+SSH_PORT=22
+
+# Set error handling.
+set -e
+
+# Add and commit changes
+git add .
+git commit -m 'deploying to production'
+git push
+
+# Deploy to production server kill previous process and start new!
+ssh -t -t -p $SSH_PORT root@$SERVER_IP <<EOF
+  cd mcg
+  # git reset --hard
+  git stash
+  git pull $REPO_URL $BRANCH
+  pnpm i
+  pm2 stop app
+  sleep 1
+  pm2 delete app
+  sleep 1
+  pkill -f "pnpm run start"   
+  sleep 1
+  pm2 start "pnpm run start" --name app
+  pm2 save
+  pm2 logs
+EOF
